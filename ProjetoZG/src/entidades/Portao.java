c78d46b2ef1b87a2e11e04011005626244283f80
Fernando Amorim dos Santos Baptista Neto <fernandoadsbn@gmail.com>
2018-08-08 01:31:50 -0300

    TestePortao

---
+++ ProjetoZG/src/entidades/Portao.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Portao {
+
+    private String status;
+    private int time;
+
+    Portao() {
+
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
